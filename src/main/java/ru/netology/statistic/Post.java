e3b801b8cbdf68fbf6bd6b148d2220d9f9071ede
Pogorelyisp <pogorelyisp@yandex.ru>
2024-05-17 17:03:58 +0300

    Initial commit

---
+++ src/main/java/ru/netology/statistic/Post.java
@@ -0,0 +1,14 @@
+package ru.netology.statistic;
+
+public class Post {
+    public FormDate birthday;    // поле ввода дня рождения
+    public String name;          // поле для имени
+    public String passport;      // поле для пасспорта
+    public String patronymic;    // поле для отчества
+    public String phone;         // поле для телефона
+    public String surname;       // поле для фамилии
+    public boolean subscription; // поле получения скидок
+
+}
+
+
